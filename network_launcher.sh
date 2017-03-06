89c1bae8654745a4428ca1fe98ac4b0883eb90bd
suryalnvs <suryalnvs@gmail.com>
2017-03-06 13:12:13 -0500

    Update network_launcher.sh

--- network_launcher.sh
+++ network_launcher.sh
@@ -10,6 +10,19 @@ function printHelp {
        ./network_launcher.sh remove is used to remove the images"
 }
 
+#function getting_tags {
+#        for i in orderer
+#        do
+#                j=peer
+#                k=ca
+#                TAG=$(curl -s -S 'https://registry.hub.docker.com/v2/repositories/rameshthoomu/fabric-'"$i"'-x86_64/tags/' | awk '{for(i=1;i<=NF;i++)if($i~/"name":/)print $(i+1)}' | grep "x86_64" | awk 'NR==1{print $1}' | tr -d "\"|,")
+#                CA_TAG=$(curl -s -S 'https://registry.hub.docker.com/v2/repositories/rameshthoomu/fabric-'"$k"'-x86_64/tags/' | awk '{for(i=1;i<=NF;i++)if($i~/"name":/)print $(i+1)}' | grep "x86_64" | awk 'NR==1{print $1}' | tr -d "\"|,")
+#                sed 's/\(.*fabric-'"$i"'-x86_64\)\(.*\)/\1:'"$TAG"'/;s/\(.*fabric-'"$j"'-x86_64\)\(.*\)/\1:'"$TAG"'/;s/\(.*fabric-'"$k"'-x86_64\)\(.*\)/\1:'"$CA_TAG"'/' docker-compose.yml > docker-compose.yml.tmp
+#                cp docker-compose.yml.tmp docker-compose.yml
+#                rm docker-compose.yml.tmp
+#        done
+#}
+
 function validateArgs {
 	if [ -z "${UP_DOWN}" ]; then
 		echo "up/down/remove not mentioned"
@@ -27,19 +40,10 @@ function validateArgs {
 validateArgs
 
 if [ "${UP_DOWN}" == "up" ]; then
-        for i in orderer
-        do
-                j=peer
-                k=ca
-                TAG=$(curl -s -S 'https://registry.hub.docker.com/v2/repositories/rameshthoomu/fabric-'"$i"'-x86_64/tags/' | awk '{for(i=1;i<=NF;i++)if($i~/"name":/)print $(i+1)}' | grep "x86_64" | awk 'NR==1{print $1}' | tr -d "\"|,")
-                CA_TAG=$(curl -s -S 'https://registry.hub.docker.com/v2/repositories/rameshthoomu/fabric-'"$k"'-x86_64/tags/' | awk '{for(i=1;i<=NF;i++)if($i~/"name":/)print $(i+1)}' | grep "x86_64" | awk 'NR==1{print $1}' | tr -d "\"|,")
-                sed 's/\(.*fabric-'"$i"'-x86_64\)\(.*\)/\1:'"$TAG"'/;s/\(.*fabric-'"$j"'-x86_64\)\(.*\)/\1:'"$TAG"'/;s/\(.*fabric-'"$k"'-x86_64\)\(.*\)/\1:'"$CA_TAG"'/' docker-compose.yml > docker-compose.yml.tmp
-                cp docker-compose.yml.tmp docker-compose.yml
-                rm docker-compose.yml.tmp
-                echo "====================Pulling CCENV Image===================="
-                docker pull rameshthoomu/fabric-ccenv-x86_64:$TAG
-                docker tag rameshthoomu/fabric-ccenv-x86_64:$TAG hyperledger/fabric-ccenv:$TAG
-        done
+        #getting_tags
+        echo "====================Pulling CCENV Image===================="
+        docker pull rameshthoomu/fabric-ccenv-x86_64:x86_64-0.7.0-snapshot-509d8ed
+        docker tag rameshthoomu/fabric-ccenv-x86_64:x86_64-0.7.0-snapshot-509d8ed hyperledger/fabric-ccenv:x86_64-0.7.0-snapshot-509d8ed
         echo "====================Starting the Network===================="
         docker-compose -f docker-compose.yml up -d
 
