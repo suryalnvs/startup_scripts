dc71ddbfd93aad9972168ef244327c544db707c0
suryalnvs <suryalnvs@gmail.com>
2017-05-15 13:24:20 -0400

    Update setup_prereq.sh

--- setup_prereq.sh
+++ setup_prereq.sh
@@ -46,6 +46,7 @@ sudo apt-get update
 sudo apt-get install -y nodejs
 
 echo "===============Installing Docker-Compose==============="
+sudo apt-get -y install curl
 sudo curl -L "https://github.com/docker/compose/releases/download/1.10.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
