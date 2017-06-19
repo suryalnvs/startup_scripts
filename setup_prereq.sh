6c593653c206b384652eeef2fffaa69d20dd107b
suryalnvs <suryalnvs@gmail.com>
2017-06-19 16:03:09 -0400

    Update setup_prereq.sh

--- setup_prereq.sh
+++ setup_prereq.sh
@@ -47,7 +47,7 @@ sudo apt-get install -y nodejs
 
 echo "===============Installing Docker-Compose==============="
 sudo apt-get -y install curl
-sudo curl -L "https://github.com/docker/compose/releases/download/1.10.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.13.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
 echo "===============Installing PIP==============="
