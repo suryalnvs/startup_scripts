f5e3ddf6de4c07b63348ded0724d677fddd0fa8c
Surya <suryalnvs@gmail.com>
2018-01-24 10:51:15 -0500

    fix setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+INSTALL_DIR=/home/$USER/eDappinstall
+
 echo "###########################################################################################"
 echo "#                                Welcome to eDapp                                         #"
 echo "###########################################################################################"
@@ -21,11 +24,17 @@ echo "#          Installing Application on AWS               #"
 echo "########################################################"
 
 export AWS_SECRET_KEY="2Oa7sUI1QmJ4yLLc9XdVq7pj5Voh12RLbdrGlx7L"
-cd ~ && git clone http://192.168.5.61:8080/eDappinstall
-cd ~/eDappinstall/ansible
-sed -i 's/ibmadmin/"${USER}"/g' ./vars/aws.yml
+if [ ! -d "$INSTALL_DIR" ]; then
+     cd ~ && git clone http://192.168.5.61:8080/eDappinstall
+else
+     cd $INSTALL_DIR
+     git checkout -- ./ansible/vars/aws.yml
+     git pull origin master
+fi
+cd $INSTALL_DIR/ansible
+sed -i "s/ibmadmin/${USER}/g" ./vars/aws.yml
 sudo ntpdate us.pool.ntp.org
-ansible-playbook -i run/runhosts -e "mode=apply env=aws cloud_type=aws" provcluster.yml
+ansible-playbook -i run/runhosts -e "mode=apply env=aws cloud_type=aws password=$AWS_SECRET_KEY" provcluster.yml
 ansible-playbook -i run/runhosts -e "mode=apply env=aws deploy_type=compose" initcluster.yml --skip-tags="resetconn"
 #ansible-playbook -i run/runhosts -e "mode=apply env=aws env_type=k8s" initcluster.yml --skip-tags="resetconn"
 ansible-playbook -i run/runhosts -e "mode=apply env=bc1st target=configtxgen,cryptogen" setupfabric.yml
